8ee12fd34be37550b70a5622b3ad4cb5593639a8
singhmahipal <mahipalsingh052025@gmail.com>
2025-09-29 21:22:01 +0530

    initial commit

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading';
+import StoriesBar from '../components/StoriesBar';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchFeeds()
+  }, [])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post */}
+      <div>
+        <StoriesBar />
+        <div className="p-4 space-y-6">
+          List of post
+        </div>
+      </div>
+
+      {/* right sidebar */}
+      <div>
+        <div>
+          <h1>sponsored</h1>
+        </div>
+        <h1>recent messages</h1>
+      </div>
+    </div>
+  ) : <Loading />
 }
 
 export default Feed
