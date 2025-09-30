cbe5ec48f3684763fa5bc2c6f836399e3469dfc3
singhmahipal <mahipalsingh052025@gmail.com>
2025-09-30 18:49:00 +0530

    initial commit

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -3,6 +3,7 @@ import { assets, dummyPostsData } from '../assets/assets';
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
+import RecentMessages from '../components/RecentMessages';
 
 const Feed = () => {
 
@@ -38,7 +39,7 @@ const Feed = () => {
           <p className="text-slate-600">Email marketing</p>
           <p className="text-slate-400">Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
         </div>
-        <h1>recent messages</h1>
+        <RecentMessages />
       </div>
     </div>
   ) : <Loading />
