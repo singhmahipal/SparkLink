0db0c96dd703314baabc9bc8a5645237b737472c
singhmahipal <mahipalsingh052025@gmail.com>
2025-10-06 16:49:26 +0530

    connected mongoDB Database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './configs/db.js';
 
 const app = express();
+
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
